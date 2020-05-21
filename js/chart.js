07d242f8257d33d6c4240abdb36ec8aa81fbf5f3
David-Glez <59298830+David-Glez@users.noreply.github.com>
2020-05-21 17:15:12 -0500

    SerialPort communication - JSON, 3 graphs

--- js/chart.js
+++ js/chart.js
@@ -1,48 +1,74 @@
 
 var counter = 0;
-var graph = [];
+var graphFlow = [];
+var graphVolume = [];
+var graphPresure = [];
 var lab = [];
 for(var i = 0; i<300; i++){
-  graph.push(0);
+  graphFlow.push(0);
+  graphVolume.push(0);
+  graphPresure.push(0);
   lab.push(i);
 }
 
+window.chartColors = {
+  red: 'rgb(255, 99, 132)', 
+  white: 'rgb(255, 255, 255)', 
+  blue: 'rgb(59, 131, 189)'
+};
 
 function onReceive(event){
   
-    let container = myChart.config.data.datasets[event.index].data.slice(0);
-    //myChart.config.data.datasets[event.index].data[counter] = event.value;
+    let containerFlow = myChartFlow.config.data.datasets[event.index].data.slice(0);
+    let containerVolume = myChartVolume.config.data.datasets[event.index].data.slice(0);
+    let containerPresure = myChartPresure.config.data.datasets[event.index].data.slice(0);
 
-    container[counter] = event.value;
-    if(counter === container.length -1){
+    containerFlow[counter] = event.f;
+    containerVolume[counter] = event.v;
+    containerPresure[counter] = event.p;
+
+    if(counter === containerFlow.length -1 && counter === containerVolume.length -1 && counter === containerPresure.length -1){
       counter = 0;
     }else{
-      container[counter + 1] = undefined;
+      containerFlow[counter + 1] = undefined;
+      containerVolume[counter +1] = undefined;
+      containerPresure[counter +1] = undefined;
       counter++;
     }
 
-    myChart.config.data.datasets[event.index].data = container;
-    myChart.update();
+    myChartFlow.config.data.datasets[event.index].data = containerFlow;
+    myChartVolume.config.data.datasets[event.index].data = containerVolume;
+    myChartPresure.config.data.datasets[event.index].data = containerPresure;
+
+    myChartFlow.update();
+    myChartVolume.update();
+    myChartPresure.update();
+    
   
 }
 
-var config = {
+var configFlow = {
   type: 'line',
   data: {
     labels: lab,
     datasets: [
       {
-        //label: 'volume',
-        backgroundColor: 'rgb(54, 162, 235)',
-        borderColor: 'rgb(54, 162, 235)',
+        backgroundColor: window.chartColors.white,
+        borderColor: window.chartColors.white,
         fill: false,
         cubicInterpolationMode: 'monotone',
-        data: graph,
-        //data:[]
+        data: graphFlow,
       },
+      
     ],
   },
   options: {
+    animation:{
+      duration:0
+    },
+    legend: {
+      display: false // Ocultar legendas
+    },
     title: {
       display: false,
       text: 'eBREATH',
@@ -57,21 +83,110 @@ var config = {
     scales: {
       xAxes: [
         {
-          /*type: 'realtime',
-          realtime: {
-            duration: 20000,
-            delay: 2000,
-            
-          },*/
           display: false
         },
       ],
       yAxes: [
         {
-          /*scaleLabel: {
-            display: true,
-            labelString: 'value',
-          },*/
+          
+        },
+      ],
+    },
+  },
+};
+
+var configVolume= {
+  type: 'line',
+  data: {
+    labels: lab,
+    datasets: [
+      
+      {
+        backgroundColor: window.chartColors.red,
+        borderColor: window.chartColors.red,
+        fill: false,
+        cubicInterpolationMode: 'monotone',
+        data: graphVolume,
+        colors: ['', 'red', 'white']
+      }
+    ],
+  },
+  options: {
+    animation:{
+      duration:0
+    },
+    legend: {
+      display: false // Ocultar legendas
+    },
+    title: {
+      display: false,
+      
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+    scales: {
+      xAxes: [
+        {
+          display: false
+        },
+      ],
+      yAxes: [
+        {
+          
+        },
+      ],
+    },
+  },
+};
+
+var configPresure= {
+  type: 'line',
+  data: {
+    labels: lab,
+    datasets: [
+      
+      {
+        backgroundColor: window.chartColors.blue,
+        borderColor: window.chartColors.blue,
+        fill: false,
+        cubicInterpolationMode: 'monotone',
+        data: graphPresure,
+        colors: ['', 'red', 'white']
+      }
+    ],
+  },
+  options: {
+    animation:{
+      duration:0
+    },
+    legend: {
+      display: false // Ocultar legendas
+    },
+    title: {
+      display: false,
+      
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+    scales: {
+      xAxes: [
+        {
+          display: false
+        },
+      ],
+      yAxes: [
+        {
+          
         },
       ],
     },
@@ -80,5 +195,9 @@ var config = {
 
 window.onload = function () {
   const ctx = document.getElementById('myChart').getContext('2d');
-  window.myChart = new Chart(ctx, config);
+  const ctx2 = document.getElementById('myChart2').getContext('2d');
+  const ctx3 = document.getElementById('myChart3').getContext('2d');
+  window.myChartFlow = new Chart(ctx, configFlow);
+  window.myChartVolume = new Chart(ctx2, configVolume);
+  window.myChartPresure = new Chart(ctx3, configPresure);
 };
--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
 const serialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
+const Delimiter = require('@serialport/parser-delimiter');
 
-const connect = (port) => {
+const esp32_port = new serialPort('/dev/ttyUSB0', {
+  baudRate: 115200, // this is synced to what was set for the Arduino Code
+  dataBits: 8, // this is the default for Arduino serial communication
+  parity: 'none', // this is the default for Arduino serial communication
+  stopBits: 1, // this is the default for Arduino serial communication
+  flowControl: false, // this is the default for Arduino serial communication
+}, function(err)
+{
+  if (err) {
+   return console.log('Error: ', err.message)
+ }
+});
+
+const parser = esp32_port.pipe(new Delimiter({ delimiter: '\n' }));
+parser.on('data', onSerialData);
+
+function onSerialData(data)
+{
+  var text = JSON.parse(data);
+  
+  console.log(text.v.toString());
+  onReceive({
+    index: 0, 
+    v: text.v.toString(),
+    f: text.f.toString(),
+    p: text.p.toString()
+  });
+  
+}
+
+/*const connect = (port) => {
   let mySerial = port.path;
 
   mySerial = new serialPort(port.path, {
@@ -16,7 +47,7 @@ const connect = (port) => {
       console.log(data);
       onReceive({
         index: 0,
-        //timestamp: Date.now(),
+       
         value: data,
       });
     });
@@ -62,4 +93,4 @@ const startConnection = () => {
     .catch((err) => console.log(err));
 };
 
-startConnection();
+startConnection();*/
